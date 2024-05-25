f8d74f9b0d7b206f2d21540e3dccc3ddc06a393c
itsyourbabySean <sherniemartinez88@gmail.com>
2024-05-25 12:57:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const btnLike1 = document.getElementById("btnLike1")
 const btnLike2 = document.getElementById("btnLike2")
-const countLike1 = document.getElementById("countLike1")
-const countLike2 = document.getElementById("countLike2")
+const countLikes1 = document.getElementById("countLikes1")
+const countLikes2 = document.getElementById("countLikes2")
 
 function hitLikes1() {
-  let totalLikes1=parseInt(countLike1.value) + 1
-  countLike1.textContent=totalLikes1.toString()
+  let totalLikes1 = parseInt(countLike1.value) + 1
+  countLike1.textContent = totalLikes1.toString()
 }
 btnLike1.addEventListener("click",hitLikes1)
