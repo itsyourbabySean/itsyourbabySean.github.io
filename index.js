37dd74fd8e45279fd7d2b6d96d6517fbf327ee06
itsyourbabySean <sherniemartinez88@gmail.com>
2024-06-08 11:30:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ function hitLikes2() {
 btnLike1.addEventListener("click",hitLikes1)
 
 function hitdisLikes1() {
-  let totalDisLikes1 = parseInt(countDisLikes1.value) + 1
-   countDiikes1.textContent = totalDisLikes1.toString()
+  let totalDisLikes1 = parseInt(countidsLikes1.value) + 1
+   countDiikes1.textContent = totaldisLikes1.toString()
 }
 btnDisLike1.addEventListener("click",DisLikes1)
 
 function hitdisLikes2() {
-  let totalDisLikes2 = parseInt(countDisLikes2.value) + 1
-   countDisLikes2.textContent = totalDisLikes2.toString()
+  let totalDisLikes2 = parseInt(countdisLikes2.value) + 1
+   countDisLikes2.textContent = totaldisLikes2.toString()
 }
-btnDisLike2.addEventListener("click",DisLikes2)
+btnDisLike2.addEventListener("click",disLikes2)
 
 const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
