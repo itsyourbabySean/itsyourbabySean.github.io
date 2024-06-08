3bbf10f03cc4c68834993968afbe09f2ef8dae9f
itsyourbabySean <sherniemartinez88@gmail.com>
2024-06-08 11:15:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const btnLike1 = document.getElementById("btnLike1")
 const btnLike2 = document.getElementById("btnLike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
-function submitComment(){
-    commentbox.textContent += comment.value.toString() + "\n"
-}
-submit.addEventListener("click",submitComment)
 
 function hitLikes1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
@@ -30,3 +26,11 @@ function DisLikes2() {
    countDisLikes2.textContent = totalDisLikes2.toString()
 }
 btnDisLike2.addEventListener("click",DisLikes2)
+
+const submit = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
+function submitComment(){
+  commentbox.textContent += comment1.value.toString() + "\n"
+}
+submit.addEventListener("click",submitComment)
